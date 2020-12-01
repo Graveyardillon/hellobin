d6ff900c370e3525f0b79cd2b89c1c5dd78af13c
Papillon6814 <so1rou.k@gmail.com>
2020-12-01 11:15:10 +0900

    create hello bin

---
+++ hello/target/release/deps/hello.d
@@ -0,0 +1,5 @@
+/Users/kunosouichirou/Documents/GitHub/homebrew-tap/hello/target/release/deps/hello: src/main.rs
+
+/Users/kunosouichirou/Documents/GitHub/homebrew-tap/hello/target/release/deps/hello.d: src/main.rs
+
+src/main.rs:
---
+++ hello/target/release/hello.d
@@ -0,0 +1,1 @@
+/Users/kunosouichirou/Documents/GitHub/homebrew-tap/hello/target/release/hello: /Users/kunosouichirou/Documents/GitHub/homebrew-tap/hello/src/main.rs
